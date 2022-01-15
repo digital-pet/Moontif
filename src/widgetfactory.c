06932a27adb4cdb9e5e125b25727aa3185f2766e
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2022-01-14 23:25:55 -0500

    fixed a off-by-one error

--- src/widgetfactory.c
+++ src/widgetfactory.c
@@ -137,15 +137,9 @@ int CreateManagedWidgetTree(lua_State* L, int parentObj, Widget wdgParent, char*
 					continue;						// loop
 				}
 				GetRegistry(L);								// !t *k tv !t2
-				printf("Expected 4\n");
-				dumpstack(L);
 				lua_getfield(L, -1, "__widgetOrder");		// !t *k tv !t2 !t3
-				printf("Expected 5\n");
-				dumpstack(L);
 				lua_pushvalue(L, -3);						// !t *k tv !t2 !t3 tv
 				lua_rawget(L, -2);							// !t *k tv !t2 !t3 *v
-				printf("Expected 6\n");
-				dumpstack(L);
 				if (lua_type(L, -1) != LUA_TNUMBER) {		// !t *k tv !t2 !t3 iv~?
 					lua_pop(L,4);							// !t *k
 					continue;
@@ -160,10 +154,10 @@ int CreateManagedWidgetTree(lua_State* L, int parentObj, Widget wdgParent, char*
 
 				iChildCount++;
 
-				lua_pop(L, 4);							// !t *k
+				lua_pop(L, 3);							// !t *k tv
 
 			}
-			lua_pop(L, 1);								// !t
+			lua_pop(L, 1);								// !t *k
 
 		}
 
